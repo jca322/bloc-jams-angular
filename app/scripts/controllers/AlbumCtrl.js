2808471c075e2816fc4c4615ac370cd3aafab67c
jca322 <jca322@gmail.com>
2016-05-04 19:51:25 -0400

    Assignment 5 resubmitted

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -2,9 +2,6 @@
     function AlbumCtrl() {
         var albumData = angular.copy(albumPicasso);
         this.albumData = albumData;
-        for (var i = 0; i < albumData.songs.length; i++) {
-            this.albumData;
-        }
     }
     
     angular
